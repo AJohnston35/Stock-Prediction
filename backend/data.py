410b10c1cc88680928621443bf7333e46659c5ae
AJohnston35 <johnstona1102@gmail.com>
2024-03-25 11:37:17 -0400

    Completed data retrieval script

--- backend/data.py
+++ backend/data.py
@@ -1,5 +1,3 @@
-import yfinance as yf # For stock data
-
 import numpy as np # For data preprocessing
 
 import pandas as pd # For dataframes
@@ -8,107 +6,223 @@ import requests # For API requests
 from ratelimit import limits, sleep_and_retry # For rate limiting
 
 import mysql.connector # For database connection
+from sqlalchemy import create_engine
 
+import concurrent.futures # For parallel processing
 from threading import Semaphore # For concurrency control
 
 endpoint = 'stockaide-db.cluster-czcooo8241qk.us-east-2.rds.amazonaws.com'
 username = 'admin'
 password = 'mnmsstar15'
 database = 'stockaide'
+engine = create_engine(f'mysql+mysqlconnector://{username}:{password}@{endpoint}/{database}', echo=False)
+table = 'stock_data'
 
 conn = mysql.connector.connect(user=username, password=password,
                               host=endpoint, database=database)
 cursor = conn.cursor()
 
+from news_sentiment import Sentiment
 
 import ta # For generating technical indicators on market data
+
+# TODO: Make the start and end date dynamic
+
+"""create_table_query = '''CREATE TABLE stock_data (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    Ticker TEXT,
+    Date DATETIME,
+    Open FLOAT(53),
+    High FLOAT(53),
+    Low FLOAT(53),
+    Close FLOAT(53),
+    Adjusted_close FLOAT(53),
+    Volume BIGINT,
+    Weekday BIGINT,
+    Weighted_avg FLOAT(53),
+    DFF FLOAT(53),
+    volume_adi FLOAT(53),
+    volume_obv BIGINT,
+    volume_cmf FLOAT(53),
+    volume_fi FLOAT(53),
+    volume_em FLOAT(53),
+    volume_sma_em FLOAT(53),
+    volume_vpt FLOAT(53),
+    volume_vwap FLOAT(53),
+    volume_mfi FLOAT(53),
+    volume_nvi FLOAT(53),
+    volatility_bbm FLOAT(53),
+    volatility_bbh FLOAT(53),
+    volatility_bbl FLOAT(53),
+    volatility_bbw FLOAT(53),
+    volatility_bbp FLOAT(53),
+    volatility_bbhi FLOAT(53),
+    volatility_bbli FLOAT(53),
+    volatility_kcc FLOAT(53),
+    volatility_kch FLOAT(53),
+    volatility_kcl FLOAT(53),
+    volatility_kcw FLOAT(53),
+    volatility_kcp FLOAT(53),
+    volatility_kchi FLOAT(53),
+    volatility_kcli FLOAT(53),
+    volatility_dcl FLOAT(53),
+    volatility_dch FLOAT(53),
+    volatility_dcm FLOAT(53),
+    volatility_dcw FLOAT(53),
+    volatility_dcp FLOAT(53),
+    volatility_atr FLOAT(53),
+    volatility_ui FLOAT(53),
+    trend_macd FLOAT(53),
+    trend_macd_signal FLOAT(53),
+    trend_macd_diff FLOAT(53),
+    trend_sma_fast FLOAT(53),
+    trend_sma_slow FLOAT(53),
+    trend_ema_fast FLOAT(53),
+    trend_ema_slow FLOAT(53),
+    trend_vortex_ind_pos FLOAT(53),
+    trend_vortex_ind_neg FLOAT(53),
+    trend_vortex_ind_diff FLOAT(53),
+    trend_trix FLOAT(53),
+    trend_mass_index FLOAT(53),
+    trend_dpo FLOAT(53),
+    trend_kst FLOAT(53),
+    trend_kst_sig FLOAT(53),
+    trend_kst_diff FLOAT(53),
+    trend_ichimoku_conv FLOAT(53),
+    trend_ichimoku_base FLOAT(53),
+    trend_ichimoku_a FLOAT(53),
+    trend_ichimoku_b FLOAT(53),
+    trend_stc FLOAT(53),
+    trend_adx FLOAT(53),
+    trend_adx_pos FLOAT(53),
+    trend_adx_neg FLOAT(53),
+    trend_cci FLOAT(53),
+    trend_visual_ichimoku_a FLOAT(53),
+    trend_visual_ichimoku_b FLOAT(53),
+    trend_aroon_up FLOAT(53),
+    trend_aroon_down FLOAT(53),
+    trend_aroon_ind FLOAT(53),
+    trend_psar_up FLOAT(53),
+    trend_psar_down FLOAT(53),
+    trend_psar_up_indicator FLOAT(53),
+    trend_psar_down_indicator FLOAT(53),
+    momentum_rsi FLOAT(53),
+    momentum_stoch_rsi FLOAT(53),
+    momentum_stoch_rsi_k FLOAT(53),
+    momentum_stoch_rsi_d FLOAT(53),
+    momentum_tsi FLOAT(53),
+    momentum_uo FLOAT(53),
+    momentum_stoch FLOAT(53),
+    momentum_stoch_signal FLOAT(53),
+    momentum_wr FLOAT(53),
+    momentum_ao FLOAT(53),
+    momentum_roc FLOAT(53),
+    momentum_ppo FLOAT(53),
+    momentum_ppo_signal FLOAT(53),
+    momentum_ppo_hist FLOAT(53),
+    momentum_pvo FLOAT(53),
+    momentum_pvo_signal FLOAT(53),
+    momentum_pvo_hist FLOAT(53),
+    momentum_kama FLOAT(53),
+    others_dr FLOAT(53),
+    others_dlr FLOAT(53),
+    others_cr FLOAT(53),
+    Increase_Decrease BIGINT,
+    Returns FLOAT(53)
+);'''
+
+cursor.execute(create_table_query)
+conn.commit()"""
+
+tickers = pd.read_csv('backend/csv_files/stock_list.csv').Code.unique()
 
 class Stock:
     ONE_MINUTE = 60
     MAX_CALLS_PER_MINUTE = 1000
-    MAX_CONCURRENT_INSERTS = 1
-    
-    semaphore = Semaphore(MAX_CONCURRENT_INSERTS)
     
     # Class to represent a stock
-    def stock_data(self):
-        historical_data = self.ticker.history(period='10y')
+    def process_ticker(self):
+        historical_data = self.get_stock_data()
         
         if historical_data.empty:
-            print(f"No data available for {self.symbol} in the past trading week")
+            print(f"No data available for {self.ticker} in the past trading week")
         else:
-            historical_data.insert(0, 'Ticker', self.symbol)
-            historical_data.insert(1, 'Date', historical_data.index)
-            # Calculate technical indicators
-            
+            historical_data = self.add_sentiment(historical_data)
+            historical_data = self.add_interest_rates(historical_data)
             historical_data = ta.add_all_ta_features(historical_data, open='Open', high='High', low='Low', close='Close', volume='Volume', fillna=True)
             historical_data['Increase_Decrease'] = np.where(historical_data['Volume'].shift(-1) > historical_data['Volume'], 1, 0)
-            historical_data['Returns'] = historical_data['Close'].pct_change()
-            historical_data.to_sql('stock_data', con=conn, if_exists='append', index=False)
+            historical_data['Returns'] = historical_data['Close'].pct_change()*100
+            return historical_data
 
     @sleep_and_retry
     @limits(calls=MAX_CALLS_PER_MINUTE, period=ONE_MINUTE)
-    def access_rate_limited_api(self):
-        print("Retrieving sentiment data...")
+    def get_stock_data(self):
         url = f'https://eodhd.com/api/eod/{self.ticker}.US?api_token=65f5adcce659e6.99552900&fmt=json&from={self.start_date}&to={self.end_date}'
         response = requests.get(url)
         data = response.json()
         df = pd.DataFrame()
-        for item in data.get(f"{self.ticker}.US", []):
+        for item in data:
             date = item['date']
-            count = item['count']
-            normalized = item['normalized']
-            new_row = {'ticker': self.ticker, 'date': date, 'count': count, 'normalized': normalized}
+            open = item['open']
+            high = item['high']
+            low = item['low']
+            close = item['close']
+            adjusted_close = item['adjusted_close']
+            volume = item['volume']
+            new_row = {'Ticker': self.ticker, 'Date': date, 'Open': open, 'High': high, 'Low': low, 'Close': close, 'Adjusted_close': adjusted_close, 'Volume': volume}
             new_row = pd.DataFrame(new_row, index=[0])
             df = pd.concat([df, new_row], ignore_index=True)
         
         return df 
     
-    def stock_info(self):
-        info = self.ticker.info
-        print(info)
+    def add_interest_rates(self, df):
+    # Load the interest rates CSV file into a DataFrame
+        interest_df = pd.read_csv('backend/csv_files/extrapolated_interest_rates.csv')
+        interest_df['DATE'] = pd.to_datetime(interest_df['DATE'])
+        # Merge the main DataFrame with the interest rates DataFrame based on the DATE column
+        merged_df = pd.merge(df, interest_df, how='left', left_on='Date', right_on='DATE')
+
+        # Drop the extra DATE column and rename FEDFUNDS to interest_rates
+        merged_df.drop(columns=['DATE'], inplace=True)
+        merged_df.rename(columns={'FEDFUNDS': 'interest_rates'}, inplace=True)
+
+        return merged_df
         
-    def add_sentiment(self):
-        print("Hello")
-        # FILL IN GAPS IN SENTIMENT DATA
-        # CREATE COLUMN "WEIGHTED AVERAGE" FOR ENTIRE DATASET
-        # ASSIGN WEEKDAY VALUE TO EACH DAY
-        # THEN TAKE AVERAGE OF FRIDAY - SUNDAY REPORTS
-        # MERGE WITH STOCK DATA
+    def add_sentiment(self, df):
+        obj = Sentiment(self.ticker, self.start_date, self.end_date)
+        sentiment_df = obj.process_ticker()
+        df['Date'] = pd.to_datetime(df['Date'])
+        df = pd.merge(df, sentiment_df, how='left', on='Date')
+        df = df.drop(columns=['Ticker_y'])
+        df = df.rename(columns={'Ticker_x': 'Ticker'})
+        return df
     
-    def write_to_database(self, df):
-        print("Writing to database...")
-        
-        with self.semaphore:
-            # Convert DataFrame rows to tuples
-            data_tuples = [tuple(row) for row in df.to_numpy()]
-            # Execute the INSERT INTO statement for each row in the DataFrame
-            cursor.executemany('''INSERT INTO sentiment_data (ticker, date, weekday, weighted_avg) VALUES (%s, %s, %s, %s)''', data_tuples)
-            conn.commit()
-    
-    def __init__(self, symbol, name, ticker, start_date, end_date):
-        self.symbol = symbol
-        self.name = name
+    def __init__(self, ticker, start_date, end_date):
         self.ticker = ticker
         self.start_date = start_date
         self.end_date = end_date
 
     
 def main():
-    print("Retrieving stock data")
-    df = pd.read_sql_query("SELECT * FROM sentiment_data ORDER BY ticker", conn)
-
-    """df = pd.read_csv('csv_files/stock_list.csv')
-
-    i = 0
-    for index, row in df.iterrows():
-        ticker = row['Code']
-        name = row['Name']
-        obj = Stock(ticker, name)
-        obj.stock_data()
-        i += 1 
-        print(str(i) + " out of " + str(len(df)))
-        """
+    start_date = '2014-03-24'
+    end_date = '2024-03-24'
+    max_threads = 75  # Adjust this based on your system's capabilities 
+    all_dfs = pd.DataFrame()
+    # Use ThreadPoolExecutor to create a pool of threads
+    with concurrent.futures.ThreadPoolExecutor(max_workers=max_threads) as executor:
+        # Submit tasks for each ticker to the executor
+        futures = {executor.submit(Stock(ticker, start_date, end_date).process_ticker): ticker for ticker in tickers}
+        processed = 0
+        total = len(futures)
+        # Iterate over completed futures and print results
+        for future in concurrent.futures.as_completed(futures):
+            result = future.result()
+            if result is not None:
+                all_dfs = pd.concat([all_dfs, result], ignore_index=True)
+            processed += 1
+            print(f'{processed}/{total} completed', end='\r')
+    
+    all_dfs.to_sql(table, con=engine, if_exists='append', index=False, chunksize=50000)
 
 if __name__ == "__main__":
     main()
--- backend/news_sentiment.py
+++ backend/news_sentiment.py
@@ -1,7 +1,3 @@
-# TODO: CHANGE STOCK DATA RETRIEVAL TO USE EODHD API
-# TODO: MERGE WITH STOCK DATA
-# TODO: MERGE INTEREST RATES WITH STOCK DATA
-# TODO: CREATE SINGLE DATABASE WITH STOCK DATA, SENTIMENT DATA, AND INTEREST RATES
 # FOR TRAINING, TAKE RANDOM SUBSET OF TICKERS ACCORDING TO PERCENTAGE COMPOSITION IN STOCK MARKET
 
 import pandas as pd # For data manipulation
@@ -35,25 +31,25 @@ class Sentiment:
     def process_ticker(self):
         try:
             # Retrieve sentiment data from the API
-            df = self.access_rate_limited_api()
+            df = self.get_sentiment_data()
             # Fill missing dates in the dataframe
             filled_df = self.fill_missing(df)
             # Sort the dataframe by date
-            filled_df = filled_df.sort_values(by=['date'])
+            filled_df = filled_df.sort_values(by=['Date'])
             # Average the sentiment data for weekends
             trading_days = self.weekend_average(filled_df)
             # Calculate the weighted average of the sentiment data
             weighted_df = self.weighted_average(trading_days, 0.7)
             # Write the data to the database
-            self.write_to_database(weighted_df)
-            print(f"Successfully processed {self.ticker}")
+            #self.write_to_database(weighted_df)
+            return weighted_df
         except Exception as e:
-            return f"Error processing {self.ticker}: {e}"
+            print(f"Error processing {self.ticker}: {e}")
+            raise e
 
     @sleep_and_retry
     @limits(calls=MAX_CALLS_PER_MINUTE, period=ONE_MINUTE)
-    def access_rate_limited_api(self):
-        print("Retrieving sentiment data...")
+    def get_sentiment_data(self):
         url = f'https://eodhd.com/api/sentiments?s={self.ticker}&from={self.start_date}&to={self.end_date}&api_token=65f5adcce659e6.99552900&fmt=json'
         response = requests.get(url)
         data = response.json()
@@ -62,7 +58,7 @@ class Sentiment:
             date = item['date']
             count = item['count']
             normalized = item['normalized']
-            new_row = {'ticker': self.ticker, 'date': date, 'count': count, 'normalized': normalized}
+            new_row = {'Ticker': self.ticker, 'Date': date, 'Count': count, 'Normalized': normalized}
             new_row = pd.DataFrame(new_row, index=[0])
             df = pd.concat([df, new_row], ignore_index=True)
         
@@ -78,41 +74,39 @@ class Sentiment:
         return dates
     
     def fill_missing(self, ticker_df):
-        print("Filling missing dates...")
         all_dates = self.generate_dates()
         for i in range(len(all_dates)):
-            if all_dates[i] not in ticker_df.date.values:
-                new_row = {'ticker': self.ticker, 'date': all_dates[i], 'count': 0, 'normalized': 0}
+            if all_dates[i] not in ticker_df.Date.values:
+                new_row = {'Ticker': self.ticker, 'Date': all_dates[i], 'Count': 0, 'Normalized': 0}
                 new_row = pd.DataFrame(new_row, index=[0])
                 ticker_df = pd.concat([ticker_df, new_row], ignore_index=True)
         return ticker_df
 
     def weekend_average(self, df):
-        print("Averaging weekend sentiment data...")
         # Convert 'date' column to datetime
-        df['date'] = pd.to_datetime(df['date'])
+        df['Date'] = pd.to_datetime(df['Date'])
 
         # Add 'weekday' column
-        df['weekday'] = df['date'].dt.dayofweek
+        df['Weekday'] = df['Date'].dt.dayofweek
 
         # Separate the weekdays and weekends
-        df_weekday = df[df['weekday'] < 4]
-        df_weekend = df[df['weekday'] >= 4]
+        df_weekday = df[df['Weekday'] < 4]
+        df_weekend = df[df['Weekday'] >= 4]
         
         rows_to_drop = []
         
         for i in range(0, len(df_weekend)):
             if i + 2 >= len(df_weekend) or i + 1 >= len(df_weekend):
                 break
-            if df_weekend.iloc[i]['weekday'] == 4:
-                sum_count = df_weekend.iloc[i]['count'] + df_weekend.iloc[i+1]['count'] + df_weekend.iloc[i+2]['count']
+            if df_weekend.iloc[i]['Weekday'] == 4:
+                sum_count = df_weekend.iloc[i]['Count'] + df_weekend.iloc[i+1]['Count'] + df_weekend.iloc[i+2]['Count']
                 if sum_count != 0:
-                    avg = ((df_weekend.iloc[i]['normalized'] * df_weekend.iloc[i]['count']) + 
-                        (df_weekend.iloc[i+1]['normalized'] * df_weekend.iloc[i+1]['count']) + 
-                        (df_weekend.iloc[i+2]['normalized'] * df_weekend.iloc[i+2]['count'])) / sum_count
+                    avg = ((df_weekend.iloc[i]['Normalized'] * df_weekend.iloc[i]['Count']) + 
+                        (df_weekend.iloc[i+1]['Normalized'] * df_weekend.iloc[i+1]['Count']) + 
+                        (df_weekend.iloc[i+2]['Normalized'] * df_weekend.iloc[i+2]['Count'])) / sum_count
                 else:
                     avg = 0
-                new_row = {'ticker': df_weekend.iloc[i]['ticker'], 'date': df_weekend.iloc[i]['date'], 'count': sum_count, 'normalized': avg, 'weekday': 4}
+                new_row = {'Ticker': df_weekend.iloc[i]['Ticker'], 'Date': df_weekend.iloc[i]['Date'], 'Count': sum_count, 'Normalized': avg, 'Weekday': 4}
                 new_row = pd.DataFrame(new_row, index=[0])
                 df_weekend = pd.concat([df_weekend, new_row], ignore_index=True)
                 rows_to_drop.extend([i, i+1, i+2])
@@ -124,24 +118,21 @@ class Sentiment:
         df_merged = pd.concat([df_weekday, df_weekend], ignore_index=True)
         
         # Sort the dataframe by 'ticker' and 'date'
-        df_merged.sort_values(['ticker', 'date'], inplace=True)
+        df_merged.sort_values(['Ticker', 'Date'], inplace=True)
 
         return df_merged
 
     def weighted_average(self, df, weight):
-        print("Calculating weighted average...")
-        df['weighted_avg'] = (df['normalized'] * weight) + (df['count']  * (1 - weight))
-        df.drop(['count', 'normalized'], axis=1, inplace=True)
+        df['Weighted_avg'] = (df['Normalized'] * weight) + (df['Count']  * (1 - weight))
+        df.drop(['Count', 'Normalized'], axis=1, inplace=True)
         return df
     
     def write_to_database(self, df):
-        print("Writing to database...")
-        
         with self.semaphore:
             # Convert DataFrame rows to tuples
             data_tuples = [tuple(row) for row in df.to_numpy()]
             # Execute the INSERT INTO statement for each row in the DataFrame
-            cursor.executemany('''INSERT INTO sentiment_data (ticker, date, weekday, weighted_avg) VALUES (%s, %s, %s, %s)''', data_tuples)
+            cursor.executemany('''INSERT INTO sentiment_data (Ticker, Date, Weekday, Weighted_avg) VALUES (%s, %s, %s, %s)''', data_tuples)
             conn.commit()
         
     def __init__(self, ticker, start_date, end_date):
@@ -149,48 +140,3 @@ class Sentiment:
         self.start_date = start_date
         self.end_date = end_date
     
-
-def main():
-    # Define the maximum number of threads
-    """df = pd.read_csv('csv_files/stock_list.csv')
-    tickers = df.Code.unique()"""
-    
-    tickers = ['AAPL','NVDA','KO']
-    start_date = '2014-03-19'
-    end_date = '2024-03-20'
-    
-    max_threads = 3  # Adjust this based on your system's capabilities
-    
-    create_table_query = '''
-    CREATE TABLE IF NOT EXISTS sentiment_data (
-        id INT AUTO_INCREMENT PRIMARY KEY,
-        ticker VARCHAR(10),
-        date DATE,
-        weekday INT,
-        weighted_avg FLOAT
-    )
-    '''
-    cursor.execute(create_table_query)
-    conn.commit()
-    
-    # Use ThreadPoolExecutor to create a pool of threads
-    with concurrent.futures.ThreadPoolExecutor(max_workers=max_threads) as executor:
-        # Submit tasks for each ticker to the executor
-        futures = {executor.submit(Sentiment(ticker, start_date, end_date).process_ticker): ticker for ticker in tickers}
-        
-        # Iterate over completed futures and print results
-        for future in concurrent.futures.as_completed(futures):
-            result = future.result()
-            print(result)  
-                
-    query = '''SELECT * FROM sentiment_data'''
-    cursor.execute(query)
-    cursor.fetchall()
-    rows = cursor.fetchall()
-    print(rows)
-    # Close the database connection
-    conn.close()
-    
-
-if __name__ == "__main__":
-    main()
